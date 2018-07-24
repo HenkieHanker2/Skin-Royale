27dd085ddd50c1b2938a31c3505a402df28dae6a
Benjamin Dover <33605947+Roshly@users.noreply.github.com>
2018-07-24 16:40:17 -0400

    Uploaded files

---
+++ SkinRoyale/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: AssemblyTitle("SkinRoyale")]
+[assembly: AssemblyDescription("The best fortnite skin checker on the market")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ronin Development")]
+[assembly: AssemblyProduct("SkinRoyale")]
+[assembly: AssemblyCopyright("Copyright © Ronin is gay Development 2018")]
+[assembly: AssemblyTrademark("Ronin Development")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7953f5a3-2bf2-481e-be5e-ff5610601ae1")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SkinRoyale/Properties/Resources.Designer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace SkinRoyale.Properties
+{
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+	[DebuggerNonUserCode]
+	[CompilerGenerated]
+	internal class Resources
+	{
+		internal Resources()
+		{
+		}
+
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				bool flag = Resources.resourceMan == null;
+				if (flag)
+				{
+					ResourceManager temp = new ResourceManager("SkinRoyale.Properties.Resources", typeof(Resources).Assembly);
+					Resources.resourceMan = temp;
+				}
+				return Resources.resourceMan;
+			}
+		}
+
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return Resources.resourceCulture;
+			}
+			set
+			{
+				Resources.resourceCulture = value;
+			}
+		}
+
+		private static ResourceManager resourceMan;
+
+		private static CultureInfo resourceCulture;
+	}
+}
